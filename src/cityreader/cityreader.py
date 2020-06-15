28c3ac134eaa545ece07c810ecb5e3c8485c0870
JC8747 <jonathan.chiaverini@gmail.com>
2020-06-14 21:38:54 -0400

    Completed MVP & Stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,21 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
+
+import csv
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -18,9 +28,18 @@ cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
+  # For each city record, create a new City instance and add it to the
   # `cities` list
-    
+  with open('cities.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            cities.append(City(row[0], float(row[3]),float(row[4])))
+            line_count += 1
+
     return cities
 
 cityreader(cities)
@@ -32,10 +51,10 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -59,13 +78,37 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+def find_cities():
+  print("Find all cities within Latitude/Longitude boundaries.")
+  point_1 = input("Enter coordinates for starting point in LAT,LON format\n")
+  point_2 = input("Enter coordinates for the opposite corner of the search box in LAT,LON format\n")
+
+# TODO Ensure that the lat and lon valuse are all floats
+# Go through each city and check to see if it falls within
+# the specified coordinates.
+
+  float_point_1 = [float(value.strip()) for value in point_1.split(",")]
+  float_point_2 = [float(value.strip()) for value in point_2.split(",")]
+
+  global cities
+  search_result = cityreader_stretch(float_point_1[0], float_point_1[1], float_point_2[0], float_point_2[1], cities)
+
+  if len(search_result) > 0:
+    print("Found the following cities within search area:\n")
+
+    for city in search_result:
+      print(city)
+
+  else:
+    print("Found no cities within search area.\n")
+
+  return search_result
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
+ within = [city for city in cities if (city.lat >= min(lat1, lat2)) and (city.lat <= max(lat1, lat2)) and (city.lon >= min(lon1, lon2)) and (city.lon <= max(lon1, lon2))]
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+ return within
 
-  return within
+#Comment out to run test script, remove comment to run in command line
+#find_cities()
